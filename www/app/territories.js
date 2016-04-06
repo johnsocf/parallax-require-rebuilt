e61ddf91f05492879eb2d1d8fb63dcbec2fa8e46
Catherine Johnson <cfjohnson@earthlink.net>
2016-04-06 11:56:07 -0600

    built modal

--- www/app/territories.js
+++ www/app/territories.js
@@ -2,19 +2,72 @@ define(function(require) {
     'use strict';
 
     var carousel = require("carousel");
-    var $ = require("jquery");
+    var $ = require("jquery"),
+        dom;
 
+    function init() {
+        cacheDom();
+        buildCarousel();
+        attachHandlers();
+    }
 
-    $(".territories-carousel .slides").each(function(){
-        console.log($(this));
-        carousel.create({
-            root : $(this),
-            hasPagination : false,
-            changeCallback : postSlideChange
+    function cacheDom() {
+        dom = {};
+        dom.viewMap = $('.view-map');
+        dom.territoriesCarousel = $(".territories-carousel");
+        dom.territoriesCarouselSlides = dom.territoriesCarousel.find('.slides');
+        dom.territoriesCarouselModal = $('.territories-carousel-modal');
+    }
+
+    function buildCarousel() {
+        dom.territoriesCarouselSlides.each(function(){
+            console.log($(this));
+            carousel.create({
+                root : $(this),
+                hasPagination : false,
+                changeCallback : postSlideChange
+            });
         });
-    });
+    }
+
+    function attachHandlers() {
+        dom.viewMap.on('click', queModal);
+    }
+
+    function attachDelegatedHandlers() {
+        console.log('attach delegated handlers');
+        dom.modalCloseButton = dom.territoriesCarouselModal.find('.close');
+
+        console.log(dom.modalCloseButton);
+
+        dom.modalCloseButton.on('click', closeModal);
+    }
 
     function postSlideChange() {
         console.log('slide changed');
     }
+
+    function queModal() {
+        var element = $(this),
+            slide = element.parents('.slide'),
+            modalInfo = slide.find('.modal-info').html();
+
+        console.log(modalInfo);
+        console.log(dom.territoriesCarouselModal);
+
+        dom.territoriesCarouselModal.html(modalInfo);
+        attachDelegatedHandlers();
+        openModal();
+    }
+
+    function openModal() {
+        dom.territoriesCarouselModal.addClass('open');
+    }
+
+    function closeModal() {
+        console.log('close modal');
+        dom.territoriesCarouselModal.removeClass('open');
+    }
+
+    init();
 });
